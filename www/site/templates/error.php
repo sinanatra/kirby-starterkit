fbbb99244480901f9218475f48a5f60d84f3acf4
Giacomo Nanni <giacomo.nanni@chialab.it>
2020-07-12 18:55:54 +0200

    fix: translations

--- www/site/templates/error.php
+++ www/site/templates/error.php
@@ -1,8 +1,8 @@
 <?php snippet('head') ?>
 
 <div>
-  <h3>Il semblerait que la page que vous recherchiez n’existe pas (ou plus!).</h3>
-  <h3><a href="<?= $site->url() ?>">Retour à la page d’accueil</a></h3>
+  <h3>This page is not available.</h3>
+  <h3><a href="<?= $site->url() ?>">Go back to the homepage.</a></h3>
 </div>
 
 <?php snippet('foot') ?>
