ddcb4e204c799ae58d2b496b63146145884ee2be
Giacomo Nanni <gn.nanni@gmail.com>
2023-02-24 15:19:39 +0100

    remove jq

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
 import "./_lib/modernizr";
 import "../scss/application.scss";
-import jquery from "jquery";
-
-window.$ = window.jQuery = jquery;
 
 import Website from "./_modules/website";
 import Home from "./_modules/home";
 
-$(function () {
+
+window.addEventListener('load', function () {
   Website.signature();
   Website.init();
   Home.init();
